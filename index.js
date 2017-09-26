2b71c648a8fd3265b9c20e9f3f7b64b7d47504ca
Kari Lindberg <karisoolindberg@gmail.com>
2017-09-26 03:37:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,12 @@ var name = "Susan"
 var height = "34"
 var message = ` ${name} is ${height} inches tall`
 
+console.log( 'Hello, world!')
+console.log( 'Hello, world!')
+console.log( 'Hello, world!')
+console.log( 'Hello, world!')
+console.log( 'Hello, world!')
+console.log( 'Hello, world!')
+console.log( 'Hello, world!')
+
 module.exports = {name, height, message}
